e609479ca53947518a5da411bc631b10545f79c7
Artem Lipskiy <artemzt25@gmail.com>
2021-09-08 11:56:23 +0300

    Infinity scroll pokemons v1.1

--- src/App.tsx
+++ src/App.tsx
@@ -1,33 +1,6 @@
-import { useState, useRef, useEffect } from 'react';
+import PokemonsList from './PokemonsList';
 import './App.css';
-import PokemonCard from './PokemonCard';
-import useIntersectionObserver from './useIntersectionObserver';
 
-const pageSize = 2;
 export default function App(): JSX.Element {
-  const [count, setCount] = useState(0);
-  const ref = useRef(null);
-
-  const isBottomVisible = useIntersectionObserver(ref, {
-    threshold: 1,
-  });
-
-  useEffect(() => {
-    if (isBottomVisible) setCount(count + 1);
-  }, [isBottomVisible]);
-
-  return (
-    <div className="App">
-      {(() => {
-        const children = [];
-        for (let i = 1; i <= count * pageSize; i++) {
-          children.push(<PokemonCard key={i} id={i} />);
-        }
-        return children;
-      })()}
-      <div ref={ref} style={{ width: '100%', height: '5px' }}>
-        Bottom
-      </div>
-    </div>
-  );
+  return <PokemonsList></PokemonsList>;
 }
---
+++ src/PokemonsList.tsx
@@ -0,0 +1,32 @@
+import PokemonCard from './PokemonCard';
+import useIntersectionObserver from './useIntersectionObserver';
+import { useState, useRef, useEffect } from 'react';
+
+const pageSize = 2;
+export default function PokemonsList(): JSX.Element {
+  const [count, setCount] = useState(0);
+  const ref = useRef(null);
+
+  const isBottomVisible: unknown = useIntersectionObserver(ref, {
+    threshold: 1,
+  });
+
+  useEffect(() => {
+    if (isBottomVisible) setCount(count + 1);
+  }, [isBottomVisible]);
+
+  return (
+    <div className="App">
+      {(() => {
+        const children = [];
+        for (let i = 1; i <= count * pageSize; i++) {
+          children.push(<PokemonCard key={i} id={i} />);
+        }
+        return children;
+      })()}
+      <div ref={ref} style={{ width: '100%', height: '5px' }}>
+        Bottom
+      </div>
+    </div>
+  );
+}
