9a0ea00ade13b1066903fbc3334e6dc51b044354
Artem Lipskiy <artemzt25@gmail.com>
2021-09-09 16:20:12 +0300

    Initial commit

--- src/PokemonsList.tsx
+++ src/PokemonsList.tsx
@@ -1,45 +1,31 @@
 import { PokemonCard } from './PokemonCard';
 import useIntersectionObserver from './useIntersectionObserver';
-import { useState, useRef, useEffect, useCallback } from 'react';
+import { useState, useRef, useEffect } from 'react';
 
-const truePageSize = 20;
+const pageSize = Math.round(window.innerHeight / 300) + 1;
 
-const initialPageCount =
-  Math.floor((Math.round(window.innerHeight / 300) + 1) / truePageSize) + 1;
 type PokeListResponse = {
   count: number;
   results: Array<{ name: string; url: string }>;
 };
 
 export function PokemonsList(): JSX.Element {
-  const [curentLastPage, setCurentLastPage] = useState(initialPageCount);
-  const [pagesToUpload, setPagesToUpload] = useState<number[]>([]);
-  const [pages, setPages] = useState<PokeListResponse[]>([]);
+  const [pageNumber, setPageNumber] = useState(0);
+  const [pokemons, setPokemons] = useState<PokeListResponse>();
   const bottomRef = useRef<HTMLDivElement>(null);
-  const updatePageCount = useCallback(() => {
-    setCurentLastPage((page) => page + 1);
-  }, []);
-
-  useIntersectionObserver(bottomRef, pages.length, updatePageCount, 1);
-
-  console.log({ pagesToUpload, curentLastPage });
+  const intersectionEntry = useIntersectionObserver(bottomRef, 1);
 
   useEffect(() => {
-    console.log({ curentLastPage, length: pages.length, initialPageCount });
-    const pagesToUpload = new Array<number>(curentLastPage - pages.length)
-      .fill(pages.length)
-      .map((length, index) => length + index);
-
-    console.log({ dif: curentLastPage - pages.length });
-    setPagesToUpload(pagesToUpload);
-  }, [curentLastPage, pages]);
+    if (intersectionEntry) setPageNumber((pageNumber) => ++pageNumber);
+  }, [intersectionEntry]);
 
   useEffect(() => {
-    async function fetchPokemons(pageNumber: number) {
-      console.log(pageNumber);
+    async function fetchPokemons() {
       try {
         const response = await fetch(
-          `https://pokeapi.co/api/v2/pokemon?offset=${pageNumber}&limit=${truePageSize}`
+          `https://pokeapi.co/api/v2/pokemon?offset=0&limit=${
+            pageNumber * pageSize * 2
+          }`
         );
         const rJSON: unknown = await response.json();
         const guard = (rJSON: unknown): rJSON is PokeListResponse =>
@@ -48,56 +34,31 @@ export function PokemonsList(): JSX.Element {
           'count' in rJSON &&
           'results' in rJSON;
         if (guard(rJSON)) {
-          const page = {
+          setPokemons({
             count: rJSON.count,
             results: rJSON.results,
-          };
-          return page;
+          });
         }
       } catch (e) {
         console.log(e);
       }
-      throw new Error(`Can't fetch page ${pageNumber}`);
     }
-
-    const fetchPages = async () => {
-      try {
-        const pokemonsPagesPromise = pagesToUpload.map(async (pageNumber) => {
-          const pokemons = await fetchPokemons(pageNumber);
-
-          return {
-            pageNumber,
-            pokemons,
-          };
-        });
-
-        const pokemonsPages = await Promise.all(pokemonsPagesPromise);
-        if (pagesToUpload.length === 0) return;
-        setPages((oldPages) => {
-          const result = [...oldPages];
-
-          for (const { pokemons, pageNumber } of pokemonsPages) {
-            if (result[pageNumber]) result[pageNumber] = pokemons;
-            else result.push(pokemons);
-          }
-          return result;
-        });
-      } catch (e) {
-        console.log({ e });
-      }
-    };
-
-    fetchPages();
-  }, [pagesToUpload]);
-
-  const pokemons = pages.flat().flatMap((pokemon) => pokemon.results);
+    fetchPokemons();
+  }, [pageNumber]);
 
   return (
     <div>
-      {pokemons.map((pokemon) => (
-        <PokemonCard key={pokemon.name} name={pokemon.name} />
-      ))}
-
+      {(() => {
+        const children = [];
+        for (let i = 0; i <= pageNumber * pageSize; i++) {
+          if (pokemons !== undefined && pokemons.results[i]) {
+            children.push(
+              <PokemonCard key={i} name={pokemons.results[i].name} />
+            );
+          }
+        }
+        return children;
+      })()}
       <div ref={bottomRef} className="refElement" />
     </div>
   );
--- src/useIntersectionObserver.tsx
+++ src/useIntersectionObserver.tsx
@@ -2,25 +2,22 @@ import { RefObject, useEffect, useState } from 'react';
 
 function useIntersectionObserver(
   elementRef: RefObject<Element>,
-  listLength: number,
-  onIntersection: () => void,
   threshold = 0
-): void {
+): IntersectionObserverEntry | undefined {
   const [entry, setEntry] = useState<IntersectionObserverEntry>();
+  const updateEntry = ([entry]: IntersectionObserverEntry[]): void => {
+    setEntry(entry);
+  };
 
   useEffect(() => {
     const node = elementRef?.current;
     if (!node) return;
-    const observer = new IntersectionObserver(([entry]) => setEntry(entry), {
-      threshold,
-    });
+    const observer = new IntersectionObserver(updateEntry, { threshold });
     observer.observe(node);
 
     return () => observer.disconnect();
   }, [elementRef, threshold]);
 
-  useEffect(() => {
-    if (entry?.isIntersecting) onIntersection();
-  }, [listLength, entry, onIntersection]);
+  return entry;
 }
 export default useIntersectionObserver;
